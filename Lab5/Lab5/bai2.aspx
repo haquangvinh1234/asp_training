9bd5933a210342a0d335d3841f85a45f46add0c2
haquangvinh1234 <haquangvinhdn2@gmail.com>
2023-12-22 23:14:49 +0700

    Lab5 + Lab6

---
+++ Lab5/Lab5/bai2.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/MasterPage.Master" CodeBehind="bai2.aspx.cs" Inherits="Lab5.bai2" %>
+
+<asp:Content ID="CTHeader" runat="server" ContentPlaceHolderID="cphHeader"></asp:Content>
+<asp:Content ID="CTContent" runat="server" ContentPlaceHolderID="cphContent">
+     <style>
+    .title-monhoc{
+        font-size: 30px;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    #txtMamonhoc{
+        padding: 3px 5px 3px 5px;
+        font-size: 18px;
+        width: 50px;
+    }
+
+    input[type="text" i] {
+      padding: 3px 5px 3px 5px;
+      font-size: 18px;
+      margin-top: 10px;
+    }
+    .actions{
+        text-align: center;
+        margin-top: 20px;
+    }
+    td {
+        margin: 10px;
+    }
+    .btn{
+        padding: 3px 5px 3px 5px;
+        font-weight: 800;
+        color: red;
+        cursor: pointer;
+    }
+    .table{
+        text-align: center;
+    }
+    .table1{
+        text-align:center;
+    }
+    .gridview{
+        text-align:center;
+    }
+    #lblErorr{
+        color: red;
+        font-size: 15px;
+        text-align: center;
+    }
+    input[type='submit' i]{
+        padding: 3px 5px 3px 5px;
+        margin-top: 10px;
+    }
+</style>
+            <div class="title-monhoc">
+                 <asp:Label ID="lblmonhoc" runat="server" Text="Tìm Kiếm Môn Học"></asp:Label>
+            </div>
+            <div class="table">
+                <div class="table1">
+                <table>
+                    <tr>
+                        <td> <asp:Label ID="lblMamonhoc" runat="server" Text="Mã môn học: "></asp:Label></td>
+                        <td> <asp:TextBox ID="txtMamonhoc" runat="server"></asp:TextBox> </td>
+                        <td> <asp:Button ID="btnTimtheoma" runat="server" Text="Tìm theo mã" OnClick="btnTimtheoma_Click" /></td>
+                    </tr>
+                      <tr>
+                        <td> <asp:Label ID="lblTenmonhoc" runat="server" Text="Tên môn học: "></asp:Label></td>
+                        <td> <asp:TextBox ID="txtTenmonhoc" runat="server"></asp:TextBox> </td>
+                        <td> <asp:Button ID="btnTimTheoTen" runat="server" Text="Tìm theo tên" OnClick="btnTimTheoTen_Click" /></td>
+                    </tr>
+                </table>
+             </div>
+                <div class="erorr">
+                    <asp:Label ID="lblErorr" runat="server" Text=""></asp:Label>
+                </div>
+                <div class="gridview">
+                    <asp:GridView ID="grvDSMH" runat="server"  OnPageIndexChanging="grvDSMH_PageIndexChanging" Width="600px"  AutoGenerateColumns="False" CellPadding="4"  ForeColor="#333333" PageSize="3"  BackColor="White" AllowPaging="True" OnSelectedIndexChanged="grvDSMH_SelectedIndexChanged">
+                        <Columns>
+                            <asp:BoundField DataField="MaMH" HeaderText="Mã môn học" ReadOnly="True" />
+                            <asp:BoundField DataField="TenMH" HeaderText="Tên môn học" />
+                        </Columns>
+                    </asp:GridView>
+                </div>
+            </div>
+</asp:Content>
---
+++ Lab5/Lab5/bai2.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab5
+{
+    public partial class bai2 : System.Web.UI.Page
+    {
+        private static string chuoiKetnoi = "Data Source=ADMIN\\SQLEXPRESS;Initial Catalog=QL_SinhVien;Integrated Security=True";
+        private static SqlConnection conn = new SqlConnection(chuoiKetnoi);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+
+        private void HienThi()
+        {
+            try
+            {
+                string select = "SELECT * FROM tbl_monhoc";
+                SqlDataAdapter adapter = new SqlDataAdapter(select, conn);
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+                grvDSMH.DataSource = data;
+                grvDSMH.DataBind();
+            }catch (Exception)
+            {
+                lblErorr.Text = "Lỗi kết nối. Vui lòng kiếm tra lại";
+            }
+        }
+
+        protected void grvDSMH_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM tbl_monhoc";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, conn);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            int dong = grvDSMH.SelectedIndex;
+            int trang = grvDSMH.PageIndex;
+            txtMamonhoc.Text = dataTable.Rows[trang * 3 + dong][0].ToString();
+            txtTenmonhoc.Text = dataTable.Rows[trang * 3 + dong][1].ToString();
+        }
+
+        protected void grvDSMH_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grvDSMH.PageIndex = e.NewPageIndex;
+        }
+
+        protected void btnTimtheoma_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string search_by_id = "SELECT * FROM tbl_monhoc WHERE MaMH='" + txtMamonhoc.Text + "'";
+                SqlDataAdapter adapter = new SqlDataAdapter(search_by_id, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                grvDSMH.DataSource = dt;
+                grvDSMH.DataBind();
+                txtMamonhoc.Text = "";
+            }catch(Exception)
+            {
+                lblErorr.Text = "Lỗi. Vui lòng kiểm tra lại";
+            }
+        }
+
+        protected void btnTimTheoTen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string search_by_name = "SELECT * FROM tbl_monhoc WHERE TenMH='" + txtTenmonhoc.Text + "'";
+                SqlDataAdapter adapter = new SqlDataAdapter(search_by_name, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                grvDSMH.DataSource = dt;
+                grvDSMH.DataBind();
+                txtTenmonhoc.Text = "";
+            }
+            catch (Exception)
+            {
+                lblErorr.Text = "Lỗi. Vui lòng kiểm tra lại";
+            }
+        }
+    }
+}
