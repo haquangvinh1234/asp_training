df1ee4376177988bc6b0e9787f7e8e832a11deeb
haquangvinh1234 <haquangvinhdn2@gmail.com>
2023-12-22 23:10:14 +0700

    Lab5

---
+++ Lab5/Lab5/bai1.aspx
@@ -0,0 +1,107 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  MasterPageFile="~/MasterPage.Master" CodeBehind="bai1.aspx.cs" Inherits="Lab5.bai1" %>
+
+
+<asp:Content ID="CTHeader" runat="server" ContentPlaceHolderID="cphHeader"></asp:Content>
+<asp:Content ID="CTContent" runat="server" ContentPlaceHolderID="cphContent">
+    <style>
+    .title-monhoc{
+        font-size: 30px;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    #txtMamonhoc{
+        padding: 3px 5px 3px 5px;
+        font-size: 18px;
+        width: 50px;
+    }
+
+    input[type="text" i] {
+      padding: 3px 5px 3px 5px;
+      font-size: 18px;
+      margin-top: 10px;
+    }
+    .actions{
+        text-align: center;
+        margin-top: 20px;
+    }
+    td {
+        margin: 10px;
+    }
+    .btn{
+        padding: 3px 5px 3px 5px;
+        font-weight: 800;
+        color: red;
+        cursor: pointer;
+    }
+    .table{
+        text-align: center;
+    }
+    .table1{
+        text-align:center;
+    }
+    .gridview{
+        text-align:center;
+    }
+    #lblErorr{
+        color: red;
+        font-size: 15px;
+        text-align: center;
+    }
+    .alertPanel {
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+    color: #721c24;
+    padding: 8px;
+    margin-bottom: 20px;
+    border: 1px solid transparent;
+    border-radius: .25rem;
+}
+</style>
+            <div class="title-monhoc">
+                 <asp:Label ID="lblmonhoc" runat="server" Text="Danh Sách Môn Học"></asp:Label>
+            </div>
+            <div class="table">
+                <div class="table1">
+                <table>
+                    <tr>
+                        <td> <asp:Label ID="lblMamonhoc" runat="server" Text="Mã môn học: "></asp:Label></td>
+                        <td> <asp:TextBox ID="txtMamonhoc" runat="server"></asp:TextBox> </td>
+                    </tr>
+                      <tr>
+                        <td> <asp:Label ID="lblTenmonhoc" runat="server" Text="Tên môn học: "></asp:Label></td>
+                        <td> <asp:TextBox ID="txtTenmonhoc" runat="server"></asp:TextBox> </td>
+                    </tr>
+                </table>
+             </div>
+                <div class="erorr">
+                    <asp:Label ID="lblErorr" runat="server" Text=""></asp:Label>
+                </div>
+                <div class="gridview">
+                    <asp:GridView ID="grvDSMH" runat="server" Width="600px"  AutoGenerateColumns="False"  OnPageIndexChanging="grvDSMH_PageIndexChanging" CellPadding="4"  ForeColor="#333333" PageSize="3"  BackColor="White" AllowPaging="True" OnSelectedIndexChanged="grvDSMH_SelectedIndexChanged">
+                        <Columns>
+                            <asp:BoundField DataField="MaMH" HeaderText="Mã môn học" ReadOnly="True" />
+                            <asp:BoundField DataField="TenMH" HeaderText="Tên môn học" />
+                            <asp:CommandField SelectText="Chọn" ShowSelectButton="True" />
+                        </Columns>
+                 <EditRowStyle BackColor="#2461BF" />
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#0066CC" />
+                <RowStyle BackColor="#EFF3FB" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+                    </asp:GridView>
+                </div>
+            </div>
+            <div class="group-action">
+                <div class="actions">
+                    <asp:Button ID="btnThem" runat="server" Text="Thêm" CssClass="btn" OnClick="btnThem_Click" />
+                    <asp:Button ID="btnLuu" runat="server" Text="Lưu" CssClass="btn" OnClick="btnLuu_Click"/>
+                    <asp:Button ID="btnXoa" runat="server" Text="Xoá" CssClass="btn" OnClick="btnXoa_Click" />
+                    <asp:Button ID="btnSua" runat="server" Text="Sửa" CssClass="btn" OnClick="btnSua_Click"/>
+                </div>
+            </div>
+   </asp:Content>
---
+++ Lab5/Lab5/bai1.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab5
+{
+    public partial class bai1 : System.Web.UI.Page
+    {
+        public static string chuoiKetNoi = "Data Source=ADMIN\\SQLEXPRESS;Initial Catalog=QL_SinhVien;Integrated Security=True";
+        public static SqlConnection connect = new SqlConnection(chuoiKetNoi);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+
+        public void HienThi()
+        {
+            try
+            {
+                string load = "SELECT * FROM tbl_monhoc";
+                SqlDataAdapter adapter = new SqlDataAdapter(load, connect);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                grvDSMH.DataSource = dataTable;
+                grvDSMH.DataBind();
+            }
+            catch (Exception)
+            {
+                lblErorr.Style["color"] = "red";
+                lblErorr.Text = "Kết nối thất bại";
+            }
+        }
+
+        protected void grvDSMH_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string chuoiSQL = "SELECT * FROM tbl_monhoc";
+            SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, chuoiKetNoi);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            int dong = grvDSMH.SelectedIndex;
+            int trang = grvDSMH.PageIndex;
+            txtMamonhoc.Text = dt.Rows[trang * 3 + dong][0].ToString();
+            txtTenmonhoc.Text = dt.Rows[trang * 3 + dong][1].ToString();
+        }
+
+        protected void grvDSMH_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grvDSMH.PageIndex = e.NewPageIndex;
+        }
+
+        protected void btnThem_Click(object sender, EventArgs e)
+        {
+            txtMamonhoc.Text = "";
+            txtTenmonhoc.Text = "";
+        }
+
+        protected void btnLuu_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string check = "SELECT * FROM tbl_monhoc WHERE MaMH='"+ txtMamonhoc.Text +"' OR TenMH=N'" + txtTenmonhoc.Text + "'";
+                SqlDataAdapter adapter = new SqlDataAdapter(check, connect);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                
+                if(dataTable.Rows.Count > 0)
+                {
+                    lblErorr.Style["color"] = "red";
+                    lblErorr.Text = "Dữ liệu đã tồn tại";
+                    txtMamonhoc.Text = "";
+                    txtTenmonhoc.Text = "";
+                }
+                else
+                {
+                    string insert = "INSERT INTO tbl_monhoc(MaMH, TenMH)VALUES('" + txtMamonhoc.Text + "', N'" + txtTenmonhoc.Text + "')";
+                    SqlCommand sqlCommand = new SqlCommand(insert, connect);
+                    connect.Open();
+                    sqlCommand.ExecuteNonQuery();
+                    lblErorr.Style["color"] = "blue";
+                    lblErorr.Text = "Thêm thành công";
+                    HienThi();
+                    connect.Close(); 
+                }
+            }
+            catch (Exception)
+            {
+                lblErorr.Style["color"] = "red";
+                lblErorr.Text = "Lỗi";
+            }
+          
+        }
+
+        protected void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string delete = "DELETE FROM tbl_monhoc WHERE MaMH='" + txtMamonhoc.Text + "'";
+                SqlCommand sqlCommand = new SqlCommand(delete, connect);
+                connect.Open();
+                sqlCommand.ExecuteNonQuery();
+                connect.Close();
+                lblErorr.Style["color"] = "blue";
+                lblErorr.Text = "Xoá thành công";
+                HienThi();
+                txtMamonhoc.Text = "";
+                txtTenmonhoc.Text = "";
+            }
+            catch (Exception)
+            {
+                lblErorr.Style["color"] = "red";
+                lblErorr.Text = "Lỗi";
+            }
+         
+        }
+
+        protected void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string update = "UPDATE tbl_monhoc SET TenMH=N'" + txtTenmonhoc.Text + "' WHERE MaMH='" + txtMamonhoc.Text + "'";
+                SqlCommand sqlCommand = new SqlCommand(update, connect);
+                connect.Open();
+                sqlCommand.ExecuteNonQuery();
+                connect.Close();
+                lblErorr.Style["color"] = "blue";
+                lblErorr.Text = "Sửa thành công";
+                HienThi();
+            }
+            catch(Exception)
+            {
+                lblErorr.Style["color"] = "red";
+                lblErorr.Text = "Lỗi";
+            }
+        
+        }
+    }
+}
